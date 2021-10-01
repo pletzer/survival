49e4c08c049385f820e99f31dc77fb94e9973745
Alex Pletzer <alexander@gokliya.net>
2021-10-01 14:26:06 +1300

    init commit

---
+++ simple/generateData.R
@@ -0,0 +1,26 @@
+# input parameters
+pend <- 1000         # number of days
+pdie <- 0.12/365.25 # prob to die every day (constant)
+c <- 2  # increased chance dying due to some treatment
+n <- 100 # number of patients
+
+time <- c()   # number of days until event
+status <- c() # 1=censored, 2=dead
+for (i in 1:n) {
+    tm <- 0
+    st <- 1
+    while (st == 1 & tm < pend) {
+        # compute the probability of dying today
+        p <- runif(1, 0., 1.)
+        if (p < c*pdie) {
+            # no luck, you're dead
+            st <- 2
+        }
+        tm <- tm + 1
+    }
+    time <- append(time, tm)
+    status <- append(status, st)
+}
+
+df <- data.frame(time = time, status = status)
+write.csv(df, 'simpleA.csv')
