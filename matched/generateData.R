bfb67dafe67142a6a8e044468ea002c1a0959d2f
Alex Pletzer <alexander@gokliya.net>
2021-11-28 16:14:39 +1300

    new dataset

---
+++ matched/generateData.R
@@ -0,0 +1,111 @@
+set.seed(1234)
+
+random_days_to_death <- function(random_num, cum_risk_func, tol = 1.e-4, maxiter = 1000) {
+	# root of this function gives the time to death
+	f <- function(t) (cum_risk_func(t) + log(random_num))
+	time_obj <- uniroot(f, c(1.e-6,10000), extendInt = "yes", trace = TRUE, tol = tol, maxiter = maxiter)
+	return(as.integer(time_obj$root))
+}
+
+test_random_days_to_death <- function() {
+	cum_risk_func <- function(t) (0.1 * t)
+	days_to_death <- random_days_to_death(0.3, cum_risk_func)
+	print(days_to_death)
+}
+
+n <- 100
+vax_date1 <- as.Date("2021-07-01")
+vax_date2 <- as.Date("2021-07-21")
+beg_monitoring <- as.Date("2021-01-01")
+end_monitoring <- as.Date("2021-10-01")
+
+
+cum_risk_func_unvax <- function(t) (5e-2*t)
+cum_risk_func_partial <- function(t) (1.e-3*t)
+cum_risk_func_full <- function(t) (1.e-2*t)
+
+
+id <- c()
+status <- c()
+event <- c()
+time <- c()
+group <- c()
+for(i in 1:n) {
+
+	# choose a random event date betwen beg_monitoring and vax_date1
+	dates <- seq(from = beg_monitoring, to = vax_date1, by = 1)
+	event_date <- sample(dates, 1)
+
+	# throw a dice, unvaccinated life expectancy
+	unvax_period <- as.integer(vax_date1 - event_date)
+	r <- runif(1)
+	days_to_death <- random_days_to_death(random_num = r, cum_risk_func = cum_risk_func_unvax)
+
+	if (days_to_death > unvax_period) {
+
+		id <- c(id, i)
+		status <- c(status, 1) # censored
+		event <- c(event, event_date)
+		time <- c(time, unvax_period)
+		group <- c(group, 1) # unvaccinated
+
+		partial_period <- as.integer(vax_date2 - vax_date1)
+		r <- r*exp(-cum_risk_func_unvax(unvax_period))
+		days_to_death <- random_days_to_death(random_num = r, cum_risk_func = cum_risk_func_partial)
+
+		if (days_to_death > partial_period) {
+
+			id <- c(id, i)
+			status <- c(status, 1) # censored
+			event <- c(event, event_date)
+			time <- c(time, partial_period)
+			group <- c(group, 2) # partially vaccinated
+
+			full_period <- as.integer(end_monitoring - vax_date2)
+			r <- r*exp(-cum_risk_func_partial(partial_period))
+			days_to_death <- random_days_to_death(random_num = r, cum_risk_func = cum_risk_func_full)
+
+			if (days_to_death > full_period) {
+
+				id <- c(id, i)
+				status <- c(status, 1) # censored
+				event <- c(event, event_date)
+				time <- c(time, full_period)
+				group <- c(group, 3) # fully vaccinated
+
+			} else {
+
+				# death occurred during fully vaccinated period
+				id <- c(id, i)
+				status <- c(status, 2) # dead
+				event <- c(event, event_date)
+				time <- c(time, days_to_death)
+				group <- c(group, 3) # fully vaccinated
+
+			}
+
+		} else {
+
+			# death occurred during partially vaccinated period (ie after vax_date1 but before vax_date2)
+			id <- c(id, i)
+			status <- c(status, 2) # dead
+			event <- c(event, event_date)
+			time <- c(time, days_to_death)
+			group <- c(group, 2) # partially vaccinated
+
+		}
+
+	} else {
+
+		# death occurred during the unvaccinated period
+		id <- c(id, i)
+		status <- c(status, 2) # dead
+		event <- c(event, event_date)
+		time <- c(time, days_to_death)
+		group <- c(group, 1) # unvaccinated
+
+	}
+
+}
+df <- data.frame(id = id, status = status, event_end_date = event, time = time, group = group)
+write.csv(df, 'data.csv')
--- phases/generateData.R
+++ phases/generateData.R
@@ -1,28 +1,59 @@
 # input parameters
-pend <- 1000         # number of days
+pend <- 1000        # number of days
 pdie <- 0.12/365.25 # prob to die every day (constant)
-c <- 2  # increased chance dying due to some treatment
-n <- 100 # number of patients
+c <- c(1, 2)  # increased chance dying due to some treatment
+n <- 20 # number of patients
+tswitch <- 500 # average number of days until patient switches over
 
 time <- c()   # number of days until event
 status <- c() # 1=censored, 2=dead
+group <- c()
+
+# iterate over the patients
 for (i in 1:n) {
+
     tm <- 0
     st <- 1
     # patients will drop out (censored) after tfinal days
     tfinal <- runif(1, 1., pend)
-    while (st == 1 & tm < tfinal) {
+
+    tsw <- rpois(1, tswitch)
+
+    # phase 1
+    while (st == 1 & tm < min(tsw, tfinal)) {
         # compute the probability of dying today
         p <- runif(1, 0., 1.)
-        if (p < c*pdie) {
+        if (p < c[1]*pdie) {
             # no luck, you're dead
             st <- 2
         }
-        tm <- tm + 1
+        tm <- tm + 1 # include the day of death, if the person dies
     }
     time <- append(time, tm)
     status <- append(status, st)
+    group <- append(group, 1)
+
+    # phase 2
+    if (st == 1 & tfinal > tsw) {
+        # the person is still alive
+
+        # reset time
+        tm <- 0
+        while (st == 1 & tm < tfinal - tsw) {
+            # compute the probability of dying today
+            p <- runif(1, 0., 1.)
+            if (p < c[2]*pdie) {
+                # no luck, you're dead
+                st <- 2
+            }
+            tm <- tm + 1
+        }
+        time <- append(time, tm)
+        status <- append(status, st)
+        group <- append(group, 2)
+    }
+
 }
 
-df <- data.frame(time = time, status = status)
-write.csv(df, 'dropout2.csv')
+df <- data.frame(time = time, status = status, group = group)
+write.csv(df, 'phases.csv')
